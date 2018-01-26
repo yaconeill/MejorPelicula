d022d92943228c4aa4d7d8a28b58c2589ed03261
Aymediacoán Mauleón <yaconeill.amq@gmail.com>
2018-01-26 09:01:35 +0000

    sin corregir

--- js/background.js
+++ js/background.js
@@ -1,42 +1,42 @@
 var bgImg = [
     {
         'id': 'AmericanHistory',
-        'img':'img/bg/AMHX.jpg'
+        'img':'../img/bg/AMHX.jpg'
     },
     {
         'id': 'CadenaPerpetua',
-        'img':'img/bg/cadena.jpg'
+        'img':'../img/bg/cadena.jpg'
     },
     {
         'id': 'Gladiator',
-        'img':'img/bg/gladiator.jpg'
+        'img':'../img/bg/gladiator.jpg'
     },
     {
         'id': 'CapAmericaInvierno',
-        'img':'img/bg/winterSoldier.jpg'
+        'img':'../img/bg/winterSoldier.jpg'
     },
     {
         'id': 'Vengadores',
-        'img':'img/bg/Avengers.jpg'
+        'img':'../img/bg/Avengers.jpg'
     },
     {
         'id': 'Deadpool',
-        'img':'img/bg/deadpool.jpg'
+        'img':'../img/bg/deadpool.jpg'
     },
     {
         'id': 'Interstellar',
-        'img':'img/bg/Interestelar.jpg'
+        'img':'../img/bg/Interestelar.jpg'
     },
     {
         'id': 'BladeRunner',
-        'img':'img/bg/bladeRunner.jpg'
+        'img':'../img/bg/bladeRunner.jpg'
     },
     {
         'id': 'batman',
-        'img':'img/bg/DKN.jpg'
+        'img':'../img/bg/DKN.jpg'
     },
     {
         'id': 'ElPianista',
-        'img':'img/bg/pianist.jpg'
+        'img':'../img/bg/pianist.jpg'
     }
 ];
